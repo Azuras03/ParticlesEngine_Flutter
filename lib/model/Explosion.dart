b45609189a599aca656fa684b6fc3cff490d2a54
Nicolas R <nr03russo@gmail.com>
2023-07-05 15:14:53 +0200

    Ajout d'explosions lors de l'appui pour ajouter des particules

---
+++ lib/model/Explosion.dart
@@ -0,0 +1,31 @@
+import 'dart:ui';
+
+import 'package:particlesengine/main.dart';
+
+class Explosion {
+
+  double x;
+  double y;
+  double size;
+  Color color;
+  late double time;
+  late bool good = true;
+
+  Explosion(this.x, this.y, this.size, this.color) {
+    time = ParticleEngine.timeExplosion;
+    if (time < 0) {
+      time = 1;
+    }
+  }
+
+  void update(){
+    if (!good) {
+      return;
+    }
+    time -= 0.01;
+    if(time <= 0){
+      good = false;
+      return;
+    }
+  }
+}
