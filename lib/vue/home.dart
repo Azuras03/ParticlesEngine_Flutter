8bd453f6c25899eb6ee3651ef2661f4935af09c7
Nicolas R <nr03russo@gmail.com>
2023-07-25 22:58:36 +0200

    Ajout d'animations sur l'écran d'accueil (package flutter_animate) et cleanup de cette page

--- lib/vue/home.dart
+++ lib/vue/home.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:particlesengine/vue/particle_engine.dart';
+import 'package:flutter_animate/flutter_animate.dart';
 
 class Home extends StatefulWidget {
   const Home({Key? key}) : super(key: key);
@@ -14,27 +15,24 @@ class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text("Particles Engine"),
-      ),
       body: Container(
         decoration: const BoxDecoration(
           color: Color(0xFF000000),
         ),
-        child: Row(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
           children: [
-            Expanded(
-              child: Container(
-                decoration: const BoxDecoration(
-                  color: Color(0xFF000000),
-                ),
-                child: const Center(
-                  child: Text(
-                    'Particles Engine',
-                    style: TextStyle(
-                      color: Color(0xFFFFFFFF),
-                      fontSize: 30,
-                    ),
+            Container(
+              decoration: const BoxDecoration(
+                color: Color(0xFF000000),
+              ),
+              child: const Center(
+                child: Text(
+                  'Particles Engine',
+                  style: TextStyle(
+                    color: Color(0xFFFFFFFF),
+                    fontSize: 30,
                   ),
                 ),
               ),
@@ -44,7 +42,9 @@ class _HomeState extends State<Home> {
                   Navigator.of(context).push(_createRoute(ParticleEngine()));
                 },
                 child: const Text('Start'))
-          ],
+          ].animate(interval: const Duration(milliseconds: 300))
+              .scale(curve: Curves.easeOutQuart, duration: const Duration(milliseconds: 1000))
+            .slideY(begin: 100, end: 0, curve: Curves.easeOutQuart, duration: const Duration(milliseconds: 1000)),
         ),
       ),
     );
@@ -59,7 +59,8 @@ Route _createRoute(page) {
       const end = Offset.zero;
       const curve = Curves.easeOutCubic;
 
-      final tween = Tween(begin: begin, end: end).chain(CurveTween(curve: curve));
+      final tween =
+          Tween(begin: begin, end: end).chain(CurveTween(curve: curve));
 
       return SlideTransition(
         position: animation.drive(tween),
