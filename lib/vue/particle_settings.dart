70d9d510471c533d13e28b10d655ce5ed98bc352
Nicolas R <nr03russo@gmail.com>
2023-07-29 13:16:44 +0200

    Ajout d'un SwitchListTile pour le retour haptique (false par défaut) et wrap des settings dans un ScrollView pour scroller :]

--- lib/vue/particle_settings.dart
+++ lib/vue/particle_settings.dart
@@ -20,151 +20,152 @@ class _ParticleSettingsState extends State<ParticleSettings> {
         appBar: AppBar(
           title: const Text("Paramètres des particules"),
         ),
-        body: Column(children: [
-          SwitchListTile(
-            title: const Text("Gravité"),
-            value: gravity,
-            onChanged: (bool value) {
-              setState(() {
-                ParticleEngine.gravity = value ? 1 : 0;
-                gravity = value;
-              });
-            },
-          ),
-          Slider(
-            value: ParticleEngine.gravity,
-            min: -10,
-            max: 10,
-            divisions: 20,
-            label: ParticleEngine.gravity.toString(),
-            onChanged: (double value) {
-              setState(() {
-                ParticleEngine.gravity = value;
-              });
-            },
-          ),
-          Column(
-            children: [
-              TextFormField(
-                decoration: const InputDecoration(
-                  border: OutlineInputBorder(),
-                  labelText: 'Taille des particules',
-                ),
-                keyboardType: TextInputType.number,
-                onChanged: (String value) {
-                  setState(() {
-                    ParticleEngine.particleSize = double.parse(value);
-                  });
-                },
-                initialValue: ParticleEngine.particleSize.toString(),
-              ),
-              const Text("Multiplicateur (Friction de l'air)"),
-              Slider(
-                value: ParticleEngine.friction,
-                min: 0,
-                max: 1,
-                divisions: 100,
-                label: ParticleEngine.friction.toString(),
-                onChanged: (double value) {
-                  setState(() {
-                    ParticleEngine.friction = value;
-                  });
-                },
-              ),
-            ],
-          ),
-          Column(
-            children: [
-              const Text(
-                "Nombre de particules au clic",
-                style: TextStyle(fontSize: 20),
-              ),
-              Slider(
-                value: ParticleEngine.nbParticlesClick.toDouble(),
-                min: 0,
-                max: 100,
-                divisions: 100,
-                label: ParticleEngine.nbParticlesClick.toString(),
-                onChanged: (double value) {
-                  setState(() {
-                    ParticleEngine.nbParticlesClick = value.toInt();
-                  });
-                },
-              ),
-            ],
-          ),
-          Container(
-            child: DropdownButtonFormField(
-              decoration: const InputDecoration(
-                border: OutlineInputBorder(),
-                labelText: 'Forme des particules',
-              ),
-              value: ParticleEngine.particleShape,
-              onChanged: (String? newValue) {
+        body: SingleChildScrollView(
+          child: Column(children: [
+            SwitchListTile(
+              title: const Text("Gravité"),
+              value: gravity,
+              onChanged: (bool value) {
                 setState(() {
-                  ParticleEngine.particleShape = newValue!;
+                  ParticleEngine.gravity = value ? 1 : 0;
+                  gravity = value;
                 });
               },
-              items: ParticleEngine.particleShapes
-                  .map<DropdownMenuItem<String>>((String value) {
-                return DropdownMenuItem<String>(
-                  value: value,
-                  child: Text(value,
-                      style: const TextStyle(
-                        fontSize: 20,
-                      )),
-                );
-              }).toList(),
             ),
-          ),
-          Column(
-            children: [
-              const Text(
-                "Nombre de particules au drag",
-                style: TextStyle(fontSize: 20),
-              ),
-              Slider(
-                value: ParticleEngine.nbParticlesDrag.toDouble(),
-                min: 0,
-                max: 10,
-                divisions: 10,
-                label: ParticleEngine.nbParticlesDrag.toString(),
-                onChanged: (double value) {
+            Slider(
+              value: ParticleEngine.gravity,
+              min: -10,
+              max: 10,
+              divisions: 20,
+              label: ParticleEngine.gravity.toString(),
+              onChanged: (double value) {
+                setState(() {
+                  ParticleEngine.gravity = value;
+                });
+              },
+            ),
+            Column(
+              children: [
+                TextFormField(
+                  decoration: const InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: 'Taille des particules',
+                  ),
+                  keyboardType: TextInputType.number,
+                  onChanged: (String value) {
+                    setState(() {
+                      ParticleEngine.particleSize = double.parse(value);
+                    });
+                  },
+                  initialValue: ParticleEngine.particleSize.toString(),
+                ),
+                const Text("Multiplicateur (Friction de l'air)"),
+                Slider(
+                  value: ParticleEngine.friction,
+                  min: 0,
+                  max: 1,
+                  divisions: 100,
+                  label: ParticleEngine.friction.toString(),
+                  onChanged: (double value) {
+                    setState(() {
+                      ParticleEngine.friction = value;
+                    });
+                  },
+                ),
+              ],
+            ),
+            Column(
+              children: [
+                const Text(
+                  "Nombre de particules au clic",
+                  style: TextStyle(fontSize: 20),
+                ),
+                Slider(
+                  value: ParticleEngine.nbParticlesClick.toDouble(),
+                  min: 0,
+                  max: 100,
+                  divisions: 100,
+                  label: ParticleEngine.nbParticlesClick.toString(),
+                  onChanged: (double value) {
+                    setState(() {
+                      ParticleEngine.nbParticlesClick = value.toInt();
+                    });
+                  },
+                ),
+              ],
+            ),
+            Container(
+              child: DropdownButtonFormField(
+                decoration: const InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: 'Forme des particules',
+                ),
+                value: ParticleEngine.particleShape,
+                onChanged: (String? newValue) {
                   setState(() {
-                    ParticleEngine.nbParticlesDrag = value.toInt();
+                    ParticleEngine.particleShape = newValue!;
                   });
                 },
+                items: ParticleEngine.particleShapes
+                    .map<DropdownMenuItem<String>>((String value) {
+                  return DropdownMenuItem<String>(
+                    value: value,
+                    child: Text(value,
+                        style: const TextStyle(
+                          fontSize: 20,
+                        )),
+                  );
+                }).toList(),
               ),
-            ],
-          ),
-          Column(
-            children: [
-              const Text(
-                "Sons des explosions",
-                style: TextStyle(fontSize: 20),
-              ),
-              DropDownMultiSelect(
-                onChanged: (List<String> values) {
-                  setState(() {
-                    ParticleEngine.explosionPathsSelected = values;
-                  });
-                },
-                options: ParticleEngine.explosionPaths,
-                selectedValues: ParticleEngine.explosionPathsSelected,
-                whenEmpty: "Aucun son sélectionné",
-              ),
-            ],
-          ),
-          SwitchListTile(
-            title: const Text("Transformations des particules"),
-            value: ParticleEngine.transformParticle,
-            onChanged: (bool value) {
-              setState(() {
-                ParticleEngine.transformParticle = value;
-              });
-            },
-          ),
-          /*SwitchListTile(
+            ),
+            Column(
+              children: [
+                const Text(
+                  "Nombre de particules au drag",
+                  style: TextStyle(fontSize: 20),
+                ),
+                Slider(
+                  value: ParticleEngine.nbParticlesDrag.toDouble(),
+                  min: 0,
+                  max: 10,
+                  divisions: 10,
+                  label: ParticleEngine.nbParticlesDrag.toString(),
+                  onChanged: (double value) {
+                    setState(() {
+                      ParticleEngine.nbParticlesDrag = value.toInt();
+                    });
+                  },
+                ),
+              ],
+            ),
+            Column(
+              children: [
+                const Text(
+                  "Sons des explosions",
+                  style: TextStyle(fontSize: 20),
+                ),
+                DropDownMultiSelect(
+                  onChanged: (List<String> values) {
+                    setState(() {
+                      ParticleEngine.explosionPathsSelected = values;
+                    });
+                  },
+                  options: ParticleEngine.explosionPaths,
+                  selectedValues: ParticleEngine.explosionPathsSelected,
+                  whenEmpty: "Aucun son sélectionné",
+                ),
+              ],
+            ),
+            SwitchListTile(
+              title: const Text("Transformations des particules"),
+              value: ParticleEngine.transformParticle,
+              onChanged: (bool value) {
+                setState(() {
+                  ParticleEngine.transformParticle = value;
+                });
+              },
+            ),
+            /*SwitchListTile(
             title: const Text("Rotation des particules"),
             value: ParticleEngine.rotateParticle,
             onChanged: (bool value) {
@@ -173,16 +174,26 @@ class _ParticleSettingsState extends State<ParticleSettings> {
               });
             },
           ),*/
-          SwitchListTile(
-            title: const Text("Trail des particules"),
-            value: ParticleEngine.trailParticle,
-            onChanged: (bool value) {
-              setState(() {
-                ParticleEngine.trailParticle = value;
-              });
-            },
-          ),
-        ]),
+            SwitchListTile(
+              title: const Text("Trail des particules"),
+              value: ParticleEngine.trailParticle,
+              onChanged: (bool value) {
+                setState(() {
+                  ParticleEngine.trailParticle = value;
+                });
+              },
+            ),
+            SwitchListTile(
+              title: const Text("Retour haptique"),
+              value: ParticleEngine.hapticFeedback,
+              onChanged: (bool value) {
+                setState(() {
+                  ParticleEngine.hapticFeedback = value;
+                });
+              },
+            ),
+          ]),
+        ),
         floatingActionButton: FloatingActionButton(
           child: const Icon(Icons.check),
           onPressed: () {
